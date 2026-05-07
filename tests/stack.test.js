f749ba6ea51d9fc434a6039d0b978e470ede8f6f
MienieN <153607888+MienieN@users.noreply.github.com>
2026-05-07 17:16:51 +0200

    Added a unit test: should knowingly fail.

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,9 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+// add unti test here:
+test('pop on empty stack returns undefined', () => {
+    stack.pop();                            // this builds off of the previous tests and will return "wow"
+    expect(stack.pop()).toBeUndefined();    // this fails the test because it returns "wow" rather than "undefined"
+});
